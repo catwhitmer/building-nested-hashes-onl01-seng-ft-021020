72e6d351960e1dd02d111597ef4bee8c0e70885c
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-14 20:16:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,6 +1,7 @@
 def third_challenge
 epic_tragedy = {
    :montague => {
+<<<<<<< HEAD
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
@@ -17,6 +18,52 @@ epic_tragedy = {
           {name: "Steven", age: "30", attitude: "confused"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
+=======
+      :patriarch => {
+        name: "Lord Montague",
+        age: "53"
+        },
+      :matriarch => {
+        name: "Lady Montague",
+        age: "54"
+        },
+      :hero => {
+        name: "Romeo",
+        age: "15",
+        status: "alive"
+        },
+      :hero_friends => [
+        {
+          :name => "Benvolio", 
+          :age => "17", 
+          :attitude => "worried"
+        },
+        {
+          :name => "Mercutio", 
+          :age => "18", 
+          :attitude => "hot-headed"
+        }
+      ]
+   }, 
+   :capulet => {
+      :patriarch => {
+        name: "Lord Capulet",
+        age: "50"
+        },
+      :matriarch => {
+        name: "Lady Capulet",
+        age: "51"
+        },
+      :heroine => {
+        name: "Juliet",
+        age: "15",
+        status: "alive"
+        },
+      :heroine_friends => [
+       {name: "Steven", age: "30", attitude: "confused"},
+       {name: "Nurse", age: "44", attitude: "worried"}
+    ]
+>>>>>>> a50a31a759465b32298a435f508cfe29d20a12c9
    }
 }
  
